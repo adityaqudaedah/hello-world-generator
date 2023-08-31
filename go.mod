abea4f0b1331c7db22df702f68f097ee48a64d27
Rahmat Aditya Qudaedah <adityaqudaedah@gmail.com>
2023-08-31 14:44:49 +0700

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/adityaqudaedah/hello-world-generator.git
+
+go 1.20
---
+++ hello_world_generator.go
@@ -0,0 +1,5 @@
+package go_hello_world_generator
+
+func CreateHelloWorld() string {
+    return "Hello World"
+}
